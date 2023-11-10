312af141bd47ccde62e34db3edc69aec5f79cb0a
StefanoMela <stefano.mela25@gmail.com>
2023-11-10 15:40:22 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import './assets/scss/styles.scss'
+
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+import App from './App.vue'
+
+createApp(App).mount('#app')
